fa734692a351d3f8669b996b90892e267c6828ff
savi-98 <109318583+savi-98@users.noreply.github.com>
2026-02-05 21:50:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ function handleNoClick() {
     noButton.textContent = messages[messageIndex];
     messageIndex = (messageIndex + 1) % messages.length;
     const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
-    yesButton.style.fontSize = `${currentSize * 1.1}px`;
+    yesButton.style.fontSize = `${currentSize * 1.2}px`;
 }
 
 function handleYesClick() {
